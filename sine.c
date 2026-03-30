868b213eec9c11b87492bf09f95d5ed2d4ec865f
Adam Aptowitz <64217755+Pugga12@users.noreply.github.com>
2026-03-30 18:00:14 +0000

    Make sure we free ram

--- sine.c
+++ sine.c
@@ -20,9 +20,20 @@ int16_t linInterp(int16_t* wtPtr, float x) {
     return (int16_t)(a1 + (decimal * (a2 - a1)));
 }
 
+/**
+ * Generates a new wavetable at a certain frequency, sample rate, and output length 
+ * 
+ * @param wtPtr The pointer to the wavetable to sample and interpolate from
+ * @param outputPtr The pointer to the output buffer. The new wavetable will be written here
+ * @param inputLength Number of input points
+ * @param outputLength Number of output points
+ * @param frequency The frequency of the sine wave to be created
+ * @param sampleRate How often we sample from the wavetable. This influences our phase accumulator increment and thus how many samples there are in a single cycle.
+ */
 void wtSineRemap(int16_t* wtPtr, int16_t* outputPtr, size_t inputLength, size_t outputLength, float frequency, float sampleRate) {
     float accumulator = 0.0f;
-    float phaseIncrement = (2048.0f * frequency) / sampleRate;
+    float phaseIncrement = (inputLength * frequency) / sampleRate;
+    printf("Phase accumulator increment %f samples\n", phaseIncrement);
     for (int i = 0; i < outputLength; i++) {
         outputPtr[i] = linInterp(wtPtr, accumulator);
         accumulator += phaseIncrement;
@@ -32,15 +43,15 @@ void wtSineRemap(int16_t* wtPtr, int16_t* outputPtr, size_t inputLength, size_t 
 
 int main(int argc, char const *argv[])
 {
-    int16_t* wavetablePtr = (int16_t*)malloc(sizeof(int16_t) * 8192);
+    int16_t* wavetablePtr = (int16_t*)malloc(sizeof(int16_t) * 256);
     int16_t* aNotePtr = (int16_t*)malloc(sizeof(int16_t) * 4096);
 
     if (wavetablePtr == NULL) {
         return -1;
     }
 
-    wtSineDiscretize(wavetablePtr, 8192);
-    wtSineRemap(wavetablePtr, aNotePtr, 8192, 4096, 20000.0f, 192000.0f);
+    wtSineDiscretize(wavetablePtr, 255);
+    wtSineRemap(wavetablePtr, aNotePtr, 255, 4096, 2600.0f, 44100.0f);
 
     printf("i = [");
     for (int i = 0; i < 4096; i++) {
@@ -49,10 +60,12 @@ int main(int argc, char const *argv[])
     printf("]\n");
 
     printf("j = [");
-    for (int i = 0; i < 2048; i++) {
+    for (int i = 0; i < 256; i++) {
         printf("(%i, %i),", i, wavetablePtr[i]);
     }
     printf("]\n");
 
+    free(wavetablePtr);
+    free(aNotePtr);
     return 0;
 }
