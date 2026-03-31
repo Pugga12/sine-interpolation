c54ea75bedb2f07f03f864f760f2bae2695f2305
Adam Aptowitz <64217755+Pugga12@users.noreply.github.com>
2026-03-31 22:33:54 +0000

    CMake & libfixmath

--- src/sine.c
+++ src/sine.c
@@ -5,6 +5,10 @@
 #include "oscillator.h"
 #include "wav.h"
 
+#ifndef M_PI
+#define M_PI 3.14159265358979323846264338327950288 // Define M_PI with high precision
+#endif
+
 void wtSineDiscretize(int16_t* ptr, size_t length) {
     for (int i = 0; i < length; i++) {
         float theta = (2 * M_PI) * ((float)i / length);
