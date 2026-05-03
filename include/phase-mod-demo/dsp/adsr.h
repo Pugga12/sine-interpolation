082aedcdb01c058cae52c7f0f740fd1369f38c2d
Adam Aptowitz <64217755+Pugga12@users.noreply.github.com>
2026-05-02 23:14:18 -0400

    Add some documentation

--- include/phase-mod-demo/dsp/adsr.h
+++ include/phase-mod-demo/dsp/adsr.h
@@ -28,12 +28,39 @@ typedef struct
     float y;
 } ADSR;
 
+/**
+ * Initialize the ADSR struct
+ *
+ * @param ptr Pointer to ADSR struct
+ * @param attackSeconds Attack time, in seconds (must be >=0)
+ * @param releaseSeconds Release time, in seconds (must be >=0)
+ * @param decaySeconds Decay time, in seconds (must be >= 0)
+ * @param sustainLevel The sustain amplitude, from 0.0 to 1.0
+ * @param sampleRate Sample rate of the audio, in Hz. A typical value is 44100 Hz
+ * @return true if successfully created, false if parameters are invalid
+ */
 bool initADSR(ADSR* ptr, float attackSeconds, float releaseSeconds, float decaySeconds, float sustainLevel, uint32_t sampleRate);
 
+/// Resets the ADSR state machine
 void reset(ADSR* ptr);
 
+/**
+ * Calculates the current ADSR value using linear functions.
+ * @par It is recommended to use @link adsrCalculateExp instead, as it is more realistic
+ * @par This must be called once per sample calculated
+ *
+ * @param ptr Pointer to ADSR struct
+ * @return ADSR amplitude, from 0.0 to 1.0
+ */
 float adsrCalculateLinear(ADSR* ptr);
 
+/// Sets the gate of the ADSR state machine
 void setGate(ADSR* adsr, bool gate);
 
+/**
+ * Calculates the current ADSR with a logarthmic curve for ATTACK and exponential for DECAY/RELEASE
+ *
+ * @param adsr Pointer to ADSR struct
+ * @return ADSR amplitude, from 0.0 to 1.0
+ */
 float adsrCalculateExp(ADSR* adsr);
--- src/main-fixed.c
+++ src/main-fixed.c
@@ -1,4 +1,3 @@
-#include <math.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
